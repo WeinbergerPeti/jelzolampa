620b0997156b13e2822df18523cfb8658b469ceb
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-04-05 10:12:50 +0200

    Szín váltás

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ function $All(elem)
     return document.querySelectorAll(elem);
 }
 
+/* GLOBÁLIS VÁLTOZÓK */
+var txt="";
+
+/* - */
+
 
 
 function init()
@@ -24,6 +29,8 @@ function init()
     ID("zold").addEventListener("click", zoldKattint);
     ID("piros").addEventListener("click", kezdoLampa);
     autoKiir("#bal");
+    ID("sarga").addEventListener("click", alapSargaSzin);
+    ID("sarga").addEventListener("click", szinvaltas);
 }
 
 function kezdoLampa()
@@ -54,11 +61,38 @@ function zoldKattint()
 
 }
 
-var txt=""
+
 
 function autoKiir(oldal)
 {   
     $(oldal+" .auto").innerHTML="&#128663";
     $(oldal+" .auto").style.fontSize="100px";
     $(oldal+" .auto").style.textAlign="center";
+}
+
+function alapSargaSzin()
+{
+    ID("piros").style.backgroundColor="red";
+    ID("piros").style.opacity="100%";
+    ID("sarga").style.backgroundColor="yellow";
+    ID("sarga").style.opacity="100%";
+    ID("zold").style.backgroundColor="green";
+    ID("zold").style.opacity="10%";
+}
+
+var tombSzin=["yellow","blue", "red", "green", "purple", "orange"];
+
+var szinSzam=0;
+function szinvaltas(event)
+{   
+    event.target.style.backgroundColor=tombSzin[szinSzam];
+    szinSzam++;
+     
+    console.log(szinSzam);
+        
+    if (szinSzam==tombSzin.length)
+    {
+        szinSzam=0;
+    }
+    
 }
